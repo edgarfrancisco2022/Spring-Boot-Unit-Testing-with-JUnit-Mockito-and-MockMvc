12eda4ac529c71c876f0587def66358468843e63
Edgar Pérez <101810702+edgarfrancisco2022@users.noreply.github.com>
2022-07-08 11:51:02 -0500

    add mvc and rest testing

---
+++ 3.00-starting-project/src/main/resources/insertData.sql
@@ -0,0 +1,4 @@
+insert into student(id, firstname, lastname, email_address) values(11, 'Student', 'One', 'student.one@luve2code_school.com')
+insert into student(id, firstname, lastname, email_address) values(12, 'Student', 'Two', 'student.two@luve2code_school.com')
+insert into student(id, firstname, lastname, email_address) values(13, 'Student', 'Three', 'student.three@luve2code_school.com')
+insert into student(id, firstname, lastname, email_address) values(14, 'Student', 'Four', 'student.four@luve2code_school.com')
